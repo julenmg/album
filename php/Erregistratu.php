57383b2c3c0da8e244f30ea65cd5bcc868024c2d
Julen Merino <julenmg12@gmail.com>
2015-10-04 16:27:40 +0200

    php2

--- php/Erregistratu.php
+++ php/Erregistratu.php
@@ -1,6 +1,6 @@
 <?php
-//mysql_connect("mysql.hostinger.es","u217085219_root","123456") or die(mysql_error());
-mysql_connect("localhost","root","") or die(mysql_error());
+mysql_connect("mysql.hostinger.es","u217085219_root","123456") or die(mysql_error());
+//mysql_connect("localhost","root","") or die(mysql_error());
 //$dbLink = new mysqli("localhost","root","","quiz");
 /*if(mysqli_connect_errno()) {
             die("MySQL connection failed: ". mysqli_connect_error());
--- php/ikuserabiltzaileak.php
+++ php/ikuserabiltzaileak.php
@@ -1,6 +1,6 @@
 <?php
-//mysql_connect("mysql.hostinger.es","u217085219_root","123456") or die(mysql_error());
-mysql_connect("localhost","root","") or die(mysql_error());
+mysql_connect("mysql.hostinger.es","u217085219_root","123456") or die(mysql_error());
+//mysql_connect("localhost","root","") or die(mysql_error());
 mysql_select_db("quiz") or die(mysql_error());
 $erabiltzaileak = mysql_query( "SELECT * FROM ERABILTZAILEA" );
 echo '<table border=1><tr><th> POSTA </th><th> IZENA </th><th> ABIZENAK </th><th> PASAHITZA </th><th> TELEFONOA </th><th> ESPEZIALITATEA </th></tr>';//<th> IRUDIA </th>
