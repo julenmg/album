955a878bd1ed25fd0b1cadf39a6c607e57b39835
Julen Merino <julenmg12@gmail.com>
2015-10-16 18:23:35 +0200

    login2

--- login.php
+++ login.php
@@ -8,7 +8,6 @@
   <link rel="icon" href="stylesPWS\images\form.png">
   <link rel="stylesheet" type="text/css" media="all" href="stylesPWS/mystyle.css">
   <link rel="stylesheet" type="text/css" media="all" href="stylesPWS/responsive.css">
-  <script type="text/javascript" src="js/myscript.js"></script>
  </head>
 
 <body>
@@ -16,26 +15,50 @@
 	<section id="container">
 		<span class="chyron"><em><a href="layout.html">&laquo; atzera</a></em></span>
 		<h2>Zure datuak sartu</h2>
-		<form name="erregistro" id="hongkiat-form" action="loginbalidatu.php" method="POST" >
+		<form name="erregistro" id="hongkiat-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  method="POST"  >
 		<div id="wrapping" class="clearfix">
 			<section id="aligned">
 
-			<!--<p>Posta-korreoa:</p><input type="email" name="POSTA" id="email" placeholder="Adb:myname001@ikasle.ehu.es" pattern="^[a-z]*[0-9]{3}\@ikasle\.ehu(\.es|\.eus)$" autocomplete="off" tabindex="2" class="txtinput" required>
+			<p>Posta-korreoa:</p><input type="email" name="POSTA" id="email" placeholder="Adb:myname001@ikasle.ehu.es" autocomplete="off" class="txtinput" >
+		
 			<p>Pasahitza:</p><input type="password" name="PASAHITZA" id="password" placeholder="Adb:123456" pattern=".{6,}"required autocomplete="off" tabindex="3" class="txtinput" required>
-			-->
-			<p>Posta-korreoa:</p><input type="email" name="POSTA" id="email" placeholder="Adb:myname001@ikasle.ehu.es"  autocomplete="off" tabindex="2" class="txtinput" required>
-			<p>Pasahitza:</p><input type="password" name="PASAHITZA" id="password" placeholder="Adb:123456" pattern=".{6,}"required autocomplete="off" tabindex="3" class="txtinput" required>
+
 			</section>
 			
+			<section id="aside" class="clearfix">
+				<section id="prioritycase">
+						<?php
+							$email = $password = "";
+							$emailEgokia=false;
+							if ($_SERVER["REQUEST_METHOD"] == "POST") {
+								$email = $_POST['POSTA'];
+								$password = $_POST['PASAHITZA'];
+								$regex = "/^[a-z]*[0-9]{3}\@ikasle\.ehu(\.es|\.eus)$/"; 
+			 
+								if (!preg_match( $regex, $email ) ){
+									echo "<h2> Sartu duzun emaila ez da egokia</h2>";
+								}else{
+									$dbLink = new mysqli("localhost","root","","quiz");
+									$erabiltzaileak = "SELECT * FROM ERABILTZAILEA WHERE EMAIL='$email' AND PASAHITZA='$password'" ;
+									$result = $dbLink->query($erabiltzaileak);
+
+									if($result->num_rows == 1)
+										header('Location: layoutBalidatuta.html');
+									else
+										echo "<h2> emaila edo pasahitza gaizki sartu duzu</h2>";
+									$dbLink->close();
+								}
+							}
+						?>
+				</section>
+			</section>
 		</div>
-		<section id='aside' class='clearfix'>
-		<h2><?php echo $_GET["id"]?></h2>
-		</section>
+
+
 		<section id="buttons">
 			<input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="LOGIN">
 			<br style="clear:both;">
 		</section>
-	
 		</form>
 	</section>
 
