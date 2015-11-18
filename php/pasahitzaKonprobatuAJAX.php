741dc385091d58e7ce638afcecef1860e9356e17
Julen Merino <julenmg12@gmail.com>
2015-11-18 10:02:57 +0100

    server

--- php/pasahitzaKonprobatuAJAX.php
+++ php/pasahitzaKonprobatuAJAX.php
@@ -1,15 +1,13 @@
 <?php
 require ('lib/nusoap.php');
 
-require_once ('lib/nusoap.php');
-
 $pasahitza = $_GET['PASAHITZA'];
 
 //Web zerbitzariaren URL-a
-//$wsdl = "http://sw14.hol.es/ServiciosWeb/comprobarcontrasena.php?wsdl";
-$wsdl = "http://localhost80/lab1/service/myserver.php";
+$wsdl = "http://wsjulenjon.holes/lab1/service/myserver.php";
+//$wsdl = "http://localhost:80/lab1/service/myserver.php";
 //Erabiltzaile objektua sortzen
-$client = new nusoap_client($wsdl, 'wsdl');
+$client = new nusoap_client($wsdl, false);
 
 //Errorern bat egon den
 $err = $client->getError();
@@ -20,12 +18,15 @@ if ($err) {
 }
 
 $erantzuna = "<h2 id='passerantzuna'  style='font-size:1.6em; margin-top:30px;";
-$result1=$client->call('konprobatu_pasahitza', array('pasahitza'=>$pasahitza));
-echo $result1 . "dfarasr";
-if(strcmp($result1,"BALIOGABEA")==0 || strcmp($pasahitza,"")==0)
+
+$result1 = $client->call('konprobatu',array('pasahitza'=>$pasahitza));
+
+if(strcmp($result1,"BALIOGABEA")==0 || strcmp($pasahitza,"")==0){
 	$erantzuna  = $erantzuna . "color:red;'>BALIOGABEA</h2>";
-else
+}
+else{
 	$erantzuna  = $erantzuna . "color:green;'>BALIOZKOA</h2>";
+}
 
 echo $erantzuna;
 ?>
--- service/myserver.php
+++ service/myserver.php
@@ -1,29 +1,27 @@
 <?php
 //call library 
 require_once ('../php/lib/nusoap.php');
-require_once('../php/lib/class.wsdlcache.php') 
+require_once('../php/lib/class.wsdlcache.php'); 
 
 
-$ns="http://localhost:80/lab1/service/myserver.php";  //nameof the service
-//$ns="http://localhost/nusoap-0.9.5/samples";  //nameof the service
+//$ns="http://localhost/lab1/service/myserver.php?wsdl";  //nameof the service
+$ns="http://wsjulenjon.holes/lab1/service/myserver.php?wsdl";  //nameof the service
 //using soap_server to create server object 
 $server = new soap_server; 
-$server->configureWSDL('konprobatu_pasahitza',$ns);
-$server->wsdl->schemaTargetNamespace=$ns
-$server->register('konprobatu_pasahitza',array('pasahitza'=>'xsd:string'),array('pasahitza'=>'xsd:string'),$ns); 
+$server->configureWSDL('konprobatu',$ns);
+$server->wsdl->schemaTargetNamespace=$ns;
+$server->register('konprobatu',array('pasahitza'=>'xsd:string'),array('erantzuna'=>'xsd:string'),$ns); 
 
 // create the function 
-function konprobatu_pasahitza($pasahitza) { 
-if(!$pasahitza){ 
-	return new soap_fault('Erab','','Pasahitza sartu!'); 
-} 
-$file = fopen("toppasswords.txt") or exit("Unable to open file!");
-//Output a line of the file until the end is reached
-	foreach($lerroa as $file){
+function konprobatu($pasahitza) { 
+	$file = fopen("toppasswords.txt","r");
+	while(!feof($file)){
+		$lerroa=trim(fgets($file));
 		if(strcmp($lerroa,$pasahitza)==0){
-			return "BALIOGABEA";
+			return "BALIOGABEA";		
 		}
-  	}
+	}
+	fclose($file);
 	return "BALIOZKOA";
 	
 } 
